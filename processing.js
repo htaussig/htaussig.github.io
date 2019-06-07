461f970e53282e2a857801fc72e9ee110bc97b04
Harry Taussig <htaussig@gmail.com>
2019-06-07 11:37:13 -0400

    a

--- processing.js
+++ processing.js
@@ -1,10 +1,7 @@
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // build script for generating processing.js
 
-console.log('out here');
-
 var Browser = {
-  console.log('processing js');
 
   isDomPresent: true,
   navigator: navigator,
@@ -789,6 +786,7 @@ module.exports = function finalizeProcessing(Processing, options) {
 
     function loadBlock(index, filename) {
       function callback(block, error) {
+        console.log("yoyoyoyoyo");
         code[index] = block;
         ++loaded;
         if (error) {
